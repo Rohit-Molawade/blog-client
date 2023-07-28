40f25ada8619fa0439838f4e24d69b5ab7c7904f
MiMic06 <rohit.molawade27@gmail.com>
2023-07-29 03:37:18 +0530

    added tailwind css and created Landing Page Header

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,24 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import Link from "next/link";
+import "../styles/globals.css";
+import { Inter, Public_Sans, Merriweather } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const Public_sans = Public_Sans({
+  display: "swap",
+  style: "normal",
+  subsets: ["latin"],
+  variable: "--font-public-sans",
+  weight: "400",
+});
+
+const merriweather = Merriweather({
+  display: "swap",
+  style: "normal",
+  subsets: ["latin"],
+  variable: "--font-merriweather",
+  weight: "400",
+});
 
 export const metadata = {
   title: "The Blog",
@@ -10,7 +27,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} ${Public_sans.variable} ${merriweather.variable}`}
+      >
+        <header className="flex pt-12 min-w-screen items-center justify-center font-public_sans text-2xl gap-12 text-slate-400) bg-black text-white">
+          <Link href={"/"} className="hover:border-b-4 pb-2 hover:text-white">
+            Home
+          </Link>
+          <Link
+            href={"/blogs"}
+            className="hover:border-b-4 pb-2 hover:text-white"
+          >
+            Blogs
+          </Link>
+          <Link
+            href={"/about"}
+            className="hover:border-b-4 pb-2 hover:text-white"
+          >
+            About
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <main className="py-40 bg-black text-white">
+        <div className="flex flex-col justify-center items-center gap-10">
+          <p className="text-8xl font-merriweather">
+            The <strong>BLOG</strong>
+          </p>
+          <p className="text-4xl p-2">Where Ideas Take Flight!</p>
+          <button className="bg-white text-black py-4 rounded-full px-6 text-lg font-bold font-public_sans">
+            <Link href={"/"}>START READING</Link>
+          </button>
+        </div>
+      </main>
+      <section>
+        <p className="flex justify-center text-black pt-12 text-4xl font-public_sans font-extrabold">
+          Latest Blogs
+        </p>
+      </section>
+    </>
   );
 }
